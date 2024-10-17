343636bfb528b0ddadd14a76c87469adcf223bdb
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-10-17 20:52:49 +0700

    membuat logo umy

--- app/src/main/java/com/example/questbasiccomposable_129/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_129/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,6 +60,12 @@ fun Praktikum2(modifier: Modifier = Modifier){
             fontStyle = FontStyle.Italic
         )
         Spacer(modifier = Modifier.padding(20.dp))
+
+        Image(
+            painter = painterResource(id =R.drawable.logo),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape)
+        )
 
     }
 }
