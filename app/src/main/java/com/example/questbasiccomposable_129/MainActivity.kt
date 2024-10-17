b5e339509a6dc3f52187284fd4b69144632fa3f6
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-10-17 20:48:06 +0700

    membuat Text ini adalah halaman login

--- app/src/main/java/com/example/questbasiccomposable_129/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_129/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,6 +50,10 @@ fun Praktikum2(modifier: Modifier = Modifier){
             fontSize = 50.sp,
             fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text( "Ini adalah halaman login",
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Italic)
 
     }
 }
